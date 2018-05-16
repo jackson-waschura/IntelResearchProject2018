7d212030bd7396da4cd153c0631eb8b7d046089c
dumbitdownjr <armanrafian@gmail.com>
2018-05-16 12:38:55 -0700

    second attempt to parse (main just for testing)

--- parse.cpp
+++ parse.cpp
@@ -1,44 +1,74 @@
-#include "utility.hpp"
 #include <iostream>
 #include <string>
 #include <map>
 
 using namespace std;
 
-const string keys =
-  "{help h usage ? | | Usage: object }"
-  "{object         |<none>| object to find }"
-  ;
+string parse(std::map<string, int> objects);
 
-string parse_input(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
+  // initialize objects (using pascal VOC)
   std::map<std::string, int> objects;
-  objects["person"] = 0;
-  objects["bicycle"] = 1;
-  objects["car"] = 2;
-  objects["motorcycle"] = 3;
-  objects["airplain"] = 4;
-  objects["bus"] = 5;
-  objects["train"] = 6;
+  objects["aeroplane"] = 1;
+  objects["airplane"] = 1;
+  objects["bicycle"] = 2;
+  objects["bird"] = 3;
+  objects["boat"] = 4;
+  objects["bottle"] = 5;
+  objects["bus"] = 6;
+  objects["car"] = 7;
+  objects["cat"] = 8;
+  objects["chair"] = 9;
+  objects["cow"] = 10;
+  objects["diningtable"] = 11;
+  objects["dog"] = 12;
+  objects["horse"] = 13;
+  objects["motorbike"] = 14;
+  objects["person"] = 15;
+  objects["potted plant"] = 16;
+  objects["sheep"] = 17;
+  objects["sofa"] = 18;
+  objects["train"] = 19;
+  objects["tv"] = 20;
+  objects["monitor"] = 20;
 
-  CommandLineParser parser(argc, argv, keys);
+  string object;
+  do {
+    object = parse(objects);
+    if (object.empty()) {
+      break;
+    }
+    std::cout << "Object " << object << " has class #" << objects[object] << endl;
+  } while (!object.empty());
 
-  // print help message
-  if (parser.has("help")) {
-    parser.printMessage();
-    return 0;
-  }
+  return 0;
+}
 
-  // check if the user typed a valid object
-  string object = parser.get<string>(object);
-  if (objects.find(object) == objects.end()) {
-    std::cerr << "Unkown object " << object << endl;
-  }
-  
-  // print errors
-  if (!parser.check()) {
-    parser.printErrors();
-    return 0;
-  }
+string parse(std::map<string, int> objects) {
+
+  string object;
+  do {
+    std::cout << "Please enter the object you wish to find: ";
+    cin >> object;
+
+    if (object == "quit" || object == "q") {
+      return std::string();
+    }
+
+    if (object == "help" || object == "h") {
+      std::cout << "Usage: <object>\n";
+      continue;
+    }
+
+    // make sure object is a known class
+    if (objects.find(object) == objects.end()) {
+      std::cout << "Unkown object type \"" << object << "\"\n";
+    }
+    else {
+      break;
+    }
+  } while (true);
 
   return object;
 }
+
